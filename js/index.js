74e620204b5c7f8f21c34a18cb0343223d5e91c5
unknown <kimberllee.al@gmail.com>
2024-12-17 07:29:00 -0500

    Completed tasks in Part 1, viewed site, read concept content, completed experiment,commented out code in index.js file.

--- js/index.js
+++ js/index.js
@@ -1,4 +1,33 @@
 // Prevent us from attempting to use variables
 // that are not declared
 "use strict"
+/*
+const myName = "Kimberly";
+console.log(myName)
+console.log(typeof myName)
 
+const eight = 8;
+console.log(eight)
+console.log(typeof eight)
+
+let isFriend = true;
+console.log(isFriend)
+console.log(typeof isFriend)
+
+isFriend = true;
+
+console.log(isFriend)
+console.log(typeof isFriend)
+
+let user = null;
+console.log(user)
+console.log(typeof user)
+
+let greeting = "Happy Holidays";
+console.log(greeting)
+console.log(typeof greeting)
+
+greeting = "Merry Christmas";
+console.log(greeting)
+console.log(typeof greeting)
+*/
